1ec275bc0657e0f0438cddd884c2ffc41435649b
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-06-05 21:48:49 -0300

    feat(styles): Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/global";
+import defaultTheme from "./styles/themes/default";
+
 function App() {
-  return <p>You'll never walk alone.</p>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <p>You'll never walk alone.</p>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
+  }
+
+  body {
+    background-color: ${(props) => props.theme["gray-900"]};
+    color: ${(props) => props.theme["gray-300"]};
+  }
+
+  body,
+  button,
+  input,
+  textarea {
+    font-family: "Roboto", sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+  }
+`;
+
+export default GlobalStyle;
