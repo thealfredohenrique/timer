e30ca464d912ac18d34411843f948be9ea219665
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-06-06 22:49:30 -0300

    feat(history): Add history page

--- src/pages/History.tsx
+++
@@ -1,5 +0,0 @@
-function History() {
-  return <h1>History</h1>;
-}
-
-export default History;
---
+++ src/pages/History/index.tsx
@@ -0,0 +1,44 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
+function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
+}
+
+export default History;
